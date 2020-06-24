3e6a4756c7de7177cf253697922f75a21d09702b
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-24 21:58:08 +0200

    FacadeUserService added

---
+++ src/main/java/pl/coderslab/charity/facade/FacadeUserService.java
@@ -0,0 +1,13 @@
+package pl.coderslab.charity.facade;
+
+import pl.coderslab.charity.dto.DonationDTO;
+import pl.coderslab.charity.dto.UserDTO;
+import java.util.Set;
+
+public interface FacadeUserService extends GenericServiceFacade<UserDTO> {
+
+    UserDTO findUserByEmail(String userEmail);
+    Integer getBagsSumByUserId(Long id);
+    Set<DonationDTO> getAllDonationsByUserId(Long id);
+
+}
