6553a4a27d3e9e7a8ab1302aab685fed10293c47
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-24 22:01:38 +0200

    GenericEntityID fixed

--- src/main/java/pl/coderslab/charity/entity/generic/GenericEntityID.java
+++ src/main/java/pl/coderslab/charity/entity/generic/GenericEntityID.java
@@ -1,7 +1,5 @@
 package pl.coderslab.charity.entity.generic;
 
-import lombok.Data;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,32 +7,31 @@ import javax.persistence.MappedSuperclass;
 import java.util.Objects;
 
 @MappedSuperclass
-@Data
 public abstract class GenericEntityID {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id);
-//    }
-//
-//    @Override
-//    public boolean equals(Object entity) {
-//        if (this == entity) return true;
-//        if (entity.getClass() != getClass()) return false;
-//        GenericEntityID that = (GenericEntityID) entity;
-//        return Objects.equals(that.id, id);
-//    }
-}
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object entity) {
+        if (this == entity) return true;
+        if (entity.getClass() != getClass()) return false;
+        GenericEntityID that = (GenericEntityID) entity;
+        return Objects.equals(that.id, id);
+    }
+
+}
