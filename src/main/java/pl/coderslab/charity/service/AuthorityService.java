834733c4b13d691f822a1c7284b51146cd478902
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-24 21:42:35 +0200

    AuthorityService space added

--- src/main/java/pl/coderslab/charity/service/AuthorityService.java
+++ src/main/java/pl/coderslab/charity/service/AuthorityService.java
@@ -4,6 +4,8 @@ import pl.coderslab.charity.entity.Authority;
 import pl.coderslab.charity.entity.generic.AuthorityType;
 import pl.coderslab.charity.service.generic.GenericService;
 
-public interface AuthorityService<T> extends GenericService<T> {
+public interface AuthorityService extends GenericService<Authority> {
+
     Authority findByName(AuthorityType name);
+
 }
