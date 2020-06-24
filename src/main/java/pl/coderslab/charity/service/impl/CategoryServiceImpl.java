f30e26ba40bafc90db9b66a0b7e680bd8de89b20
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-24 21:47:19 +0200

    CategoryServiceImpl findAll() method overridden - returned TreeSet instead HashSet

--- src/main/java/pl/coderslab/charity/service/impl/CategoryServiceImpl.java
+++ src/main/java/pl/coderslab/charity/service/impl/CategoryServiceImpl.java
@@ -1,14 +1,17 @@
-package pl.coderslab.charity.impl;
+package pl.coderslab.charity.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.coderslab.charity.entity.Category;
-import pl.coderslab.charity.impl.generic.GenericServiceImpl;
+import pl.coderslab.charity.service.impl.generic.GenericServiceImpl;
 import pl.coderslab.charity.repository.CategoryRepository;
 import pl.coderslab.charity.service.CategoryService;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 @Service
-public class CategoryServiceImpl extends GenericServiceImpl<Category, CategoryRepository> implements CategoryService<Category> {
+public class CategoryServiceImpl extends GenericServiceImpl<Category, CategoryRepository> implements CategoryService {
 
     @Autowired
     public CategoryServiceImpl(CategoryRepository repository) {
@@ -19,4 +22,10 @@ public class CategoryServiceImpl extends GenericServiceImpl<Category, CategoryRe
     public Category findByName(String name) {
         return repository.findByName(name);
     }
+
+    @Override
+    public Set<Category> findAll() {
+        return new TreeSet<>(repository.findAll());
+    }
+
 }
