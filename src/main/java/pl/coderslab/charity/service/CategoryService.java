4a09a31e619faf6121374fcc0604f34a16a12850
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-24 21:45:43 +0200

    CategoryService space added

--- src/main/java/pl/coderslab/charity/service/CategoryService.java
+++ src/main/java/pl/coderslab/charity/service/CategoryService.java
@@ -3,7 +3,8 @@ package pl.coderslab.charity.service;
 import pl.coderslab.charity.entity.Category;
 import pl.coderslab.charity.service.generic.GenericService;
 
-public interface CategoryService<T> extends GenericService<T> {
+public interface CategoryService extends GenericService<Category> {
 
     Category findByName(String name);
+
 }
