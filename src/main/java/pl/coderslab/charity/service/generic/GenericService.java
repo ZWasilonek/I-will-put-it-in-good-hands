42b4eaeb424752afc13b2f929d60091c4685ab4c
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-24 22:02:25 +0200

    GenericService List to Set and some parameters names changed

--- src/main/java/pl/coderslab/charity/service/generic/GenericService.java
+++ src/main/java/pl/coderslab/charity/service/generic/GenericService.java
@@ -1,15 +1,15 @@
 package pl.coderslab.charity.service.generic;
 
-import java.util.List;
+import java.util.Set;
 
 public interface GenericService<T> {
 
-    T create(T o);
-    T update(T t);
-    List<T> findAll();
-    List<T> findAll(int pageNo, int limit);
-    void remove(T o);
-    void removeById(Long id);
+    T create(T entity);
+    T update(T entity);
+    Set<T> findAll();
+    Set<T> findAll(int pageNo, int limit);
+    boolean remove(T entity);
+    boolean removeById(Long id);
     T findById(Long id);
 
 }
