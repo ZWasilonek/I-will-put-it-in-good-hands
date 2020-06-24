ceb313880998116ac544243e58ded7d042c84b38
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-24 21:55:51 +0200

    DonationService List to Set and getQuantitySumFromAll() name changed to getTotalBagsQuantity()

--- src/main/java/pl/coderslab/charity/service/DonationService.java
+++ src/main/java/pl/coderslab/charity/service/DonationService.java
@@ -3,12 +3,12 @@ package pl.coderslab.charity.service;
 import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.service.generic.GenericService;
 
-import java.util.List;
+import java.util.Set;
 
-public interface DonationService<T> extends GenericService<T> {
+public interface DonationService extends GenericService<Donation> {
 
-    Integer getQuantitySumFromAll();
+    Integer getTotalBagsQuantity();
     Integer getQuantitySumFromUserId(Long userId);
-    List<Donation> getDonationsByUserId(Long userId);
+    Set<Donation> getDonationsByUserId(Long userId);
 
 }
