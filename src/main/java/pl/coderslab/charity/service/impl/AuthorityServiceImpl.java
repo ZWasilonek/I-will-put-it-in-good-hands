3dffc2a88ec9396dd123cb92cb1a72c741d9bfac
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-24 21:43:05 +0200

    AuthorityServiceImpl service annotation added

--- src/main/java/pl/coderslab/charity/service/impl/AuthorityServiceImpl.java
+++ src/main/java/pl/coderslab/charity/service/impl/AuthorityServiceImpl.java
@@ -1,13 +1,15 @@
-package pl.coderslab.charity.impl;
+package pl.coderslab.charity.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pl.coderslab.charity.entity.Authority;
 import pl.coderslab.charity.entity.generic.AuthorityType;
-import pl.coderslab.charity.impl.generic.GenericServiceImpl;
+import pl.coderslab.charity.service.impl.generic.GenericServiceImpl;
 import pl.coderslab.charity.repository.AuthorityRepository;
 import pl.coderslab.charity.service.AuthorityService;
 
-public class AuthorityServiceImpl extends GenericServiceImpl<Authority, AuthorityRepository> implements AuthorityService<Authority> {
+@Service
+public class AuthorityServiceImpl extends GenericServiceImpl<Authority, AuthorityRepository> implements AuthorityService {
 
     @Autowired
     public AuthorityServiceImpl(AuthorityRepository repository) {
@@ -18,4 +20,5 @@ public class AuthorityServiceImpl extends GenericServiceImpl<Authority, Authorit
     public Authority findByName(AuthorityType name) {
         return repository.findByName(name);
     }
+
 }
