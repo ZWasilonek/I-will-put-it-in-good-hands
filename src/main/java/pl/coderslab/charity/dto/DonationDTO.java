a28f4a6a8cf226c46e9c0daadd76c027cf27cdff
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-24 21:51:36 +0200

    DonationDTO added

---
+++ src/main/java/pl/coderslab/charity/dto/DonationDTO.java
@@ -0,0 +1,26 @@
+package pl.coderslab.charity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import java.util.Set;
+import java.util.TreeSet;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Component
+public class DonationDTO {
+
+    private Long id;
+    private Integer bagsQuantity;
+    private Set<CategoryDTO> categories;
+    private InstitutionDTO institution;
+    private ShippingAddressDTO shippingAddress;
+
+    public DonationDTO() {
+        categories = new TreeSet<>();
+    }
+
+}
