33bfa83808eeb7ff410c4ac2c4c6dcf97b4caa6f
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-25 17:44:37 +0200

    ShippingAddressMapper added

---
+++ src/main/java/pl/coderslab/charity/mapper/ShippingAddressMapper.java
@@ -0,0 +1,38 @@
+package pl.coderslab.charity.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import pl.coderslab.charity.dto.ShippingAddressDTO;
+import pl.coderslab.charity.entity.ShippingAddress;
+
+@Mapper(componentModel = "spring")
+public interface ShippingAddressMapper {
+
+    @Mappings({
+            @Mapping(target = "id", source = "entity.id"),
+            @Mapping(target = "street", source = "entity.street"),
+            @Mapping(target = "city", source = "entity.city"),
+            @Mapping(target = "zipCode", source = "entity.zipCode"),
+            @Mapping(target = "pickUpDate", source = "entity.pickUpDate"),
+            @Mapping(target = "pickUpTime", source = "entity.pickUpTime"),
+            @Mapping(target = "pickUpComment", source = "entity.pickUpComment"),
+            @Mapping(target = "phoneNumber", source = "entity.phoneNumber"),
+            @Mapping(target = "addressOwner", source = "entity.user"),
+    })
+    ShippingAddressDTO mapToDTO(ShippingAddress entity);
+
+    @Mappings({
+            @Mapping(source = "dto.id", target = "id"),
+            @Mapping(source = "dto.street", target = "street"),
+            @Mapping(source = "dto.city", target = "city"),
+            @Mapping(source = "dto.zipCode", target = "zipCode"),
+            @Mapping(source = "dto.pickUpDate", target = "pickUpDate"),
+            @Mapping(source = "dto.pickUpTime", target = "pickUpTime"),
+            @Mapping(source = "dto.pickUpComment", target = "pickUpComment"),
+            @Mapping(source = "dto.phoneNumber", target = "phoneNumber"),
+            @Mapping(source = "dto.addressOwner", target = "user"),
+    })
+    ShippingAddress mapToEntity(ShippingAddressDTO dto);
+
+}
