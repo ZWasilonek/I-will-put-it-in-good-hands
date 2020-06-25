f80d177ec22b8fbe74a5cc7bf924ee7fd69e100f
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-25 17:26:25 +0200

    InstitutionRepository redundant imports removed

--- src/main/java/pl/coderslab/charity/repository/InstitutionRepository.java
+++ src/main/java/pl/coderslab/charity/repository/InstitutionRepository.java
@@ -1,11 +1,7 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import pl.coderslab.charity.entity.Institution;
-
-import java.util.List;
 
 public interface InstitutionRepository extends JpaRepository<Institution, Long> {
 
