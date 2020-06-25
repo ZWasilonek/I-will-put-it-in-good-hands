ec6790456e1d789f6257866c8ff9e3ab77f0c169
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-25 17:59:24 +0200

    UserService <T> to GenericService<User> changed

--- src/main/java/pl/coderslab/charity/service/UserService.java
+++ src/main/java/pl/coderslab/charity/service/UserService.java
@@ -1,11 +1,12 @@
 package pl.coderslab.charity.service;
 
-import pl.coderslab.charity.dto.UserDTO;
 import pl.coderslab.charity.entity.User;
 import pl.coderslab.charity.service.generic.GenericService;
 
-public interface UserService<T> extends GenericService<T> {
-    T findByEmail(String email);
-    void saveUserDTO(UserDTO userDTO);
+public interface UserService extends GenericService<User> {
+
+    User findByEmail(String email);
+    void saveUser(User user);
     void setRole(User user);
+
 }
