b55118816c7d8401fb9c21506c260835883d9d10
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-25 17:48:58 +0200

    UserController FacadeUserService added

--- src/main/java/pl/coderslab/charity/controller/UserController.java
+++ src/main/java/pl/coderslab/charity/controller/UserController.java
@@ -7,26 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.coderslab.charity.entity.Donation;
-import pl.coderslab.charity.entity.Institution;
-import pl.coderslab.charity.entity.User;
-import pl.coderslab.charity.impl.DonationServiceImpl;
-import pl.coderslab.charity.impl.UserServiceImpl;
+import pl.coderslab.charity.dto.DonationDTO;
+import pl.coderslab.charity.dto.InstitutionDTO;
+import pl.coderslab.charity.dto.UserDTO;
+import pl.coderslab.charity.facade.FacadeUserService;
 
-import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    private final UserServiceImpl userService;
-    private final DonationServiceImpl donationService;
+    private final FacadeUserService facadeService;
 
     @Autowired
-    public UserController(UserServiceImpl userService, DonationServiceImpl donationService) {
-        this.userService = userService;
-        this.donationService = donationService;
+    public UserController(FacadeUserService facadeUserService) {
+        this.facadeService = facadeUserService;
     }
 
     @GetMapping
@@ -35,33 +33,31 @@ public class UserController {
     }
 
     @ModelAttribute("userSession")
-    public User getUserFromSession() {
+    public UserDTO getUserFromSession() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (principal instanceof UserDetails) {
             String userEmail = ((UserDetails) principal).getUsername();
-            return userService.findByEmail(userEmail);
+            return facadeService.findUserByEmail(userEmail);
         }
         return null;
     }
 
     @ModelAttribute("allUserBags")
     public Integer getAllUserBags() {
-        Integer quantitySumFromUserId = donationService.getQuantitySumFromUserId(getUserFromSession().getId());
-        if (quantitySumFromUserId != null) {
-            return quantitySumFromUserId;
-        }
-        return 0;
+        Integer quantitySumFromUserId = facadeService.getBagsSumByUserId(getUserFromSession().getId());
+        return Objects.requireNonNullElse(quantitySumFromUserId, 0);
     }
 
     @ModelAttribute("userDonations")
-    public List<Donation> getAllUserDonations() {
-        return donationService.getDonationsByUserId(getUserFromSession().getId());
+    public Set<DonationDTO> getAllUserDonations() {
+        return facadeService.getAllDonationsByUserId(getUserFromSession().getId());
     }
 
     @ModelAttribute("userInstitutions")
-    public List<Institution> showUserSelectedInstitutions() {
+    public Set<InstitutionDTO> showUserSelectedInstitutions() {
         return getAllUserDonations().stream()
-                .map(Donation::getInstitution)
-                .collect(Collectors.toList());
+                .map(DonationDTO::getInstitution)
+                .collect(Collectors.toSet());
     }
+
 }
