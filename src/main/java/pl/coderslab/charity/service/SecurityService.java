3a819a92dc01547973213382d6e2ad91b4b079a3
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-25 17:36:33 +0200

    SecurityService space added

--- src/main/java/pl/coderslab/charity/service/SecurityService.java
+++ src/main/java/pl/coderslab/charity/service/SecurityService.java
@@ -1,7 +1,8 @@
 package pl.coderslab.charity.service;
 
 public interface SecurityService {
-    String findLoggedInUsername();
 
+    String findLoggedInUsername();
     void autoLogin(String username, String password);
+
 }
