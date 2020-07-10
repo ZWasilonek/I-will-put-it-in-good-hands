a62aa03493560f3d0331386fb465d6223c328f95
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-07-10 21:17:22 +0200

    charity_donation.sql changed

--- target/classes/META-INF/charity_donation.sql
+++ target/classes/META-INF/charity_donation.sql
@@ -13,15 +13,17 @@ INSERT INTO `user_authority` VALUES (2,2);
 Insert into institutions VALUES (null, 'Pomoc dzieciom z ubogich rodzin.', 'Fundacja “Dbam o Zdrowie”'), (null, 'Pomoc wybudzaniu dzieci ze śpiączki.', 'Fundacja “A kogo”');
 Insert into institutions VALUES (null, 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.', 'Fundacja “Dla dzieci”'), (null, 'Pomoc dla osób nie posiadających miejsca zamieszkania.', 'Fundacja “Bez domu”');
 
-INSERT INTO shipping_address VALUES (null, 'Warszawa', '666-354-754', 'Brak komentarza', '2020-05-15', '15:00','Kwiatowa', '04-888', 2);
-INSERT INTO shipping_address VALUES (null, 'Warszawa', '820-654-946', null, '2020-09-10', '09:00','Liliowa', '01-098', 1);
+INSERT INTO shipping_address VALUES (null, 'Warszawa', '666-354-754', 'Brak komentarza', '2020-05-15', '15:00','Kwiatowa', '04-888');
+INSERT INTO shipping_address VALUES (null, 'Warszawa', '820-654-946', null, '2020-09-10', '09:00','Liliowa', '01-098');
 
-INSERT INTO donations VALUES (null,2, 2, 1);
-INSERT INTO donations VALUES (null,3, 1, 2);
+INSERT INTO donations VALUES (null,2, 2, 1, 1);
+INSERT INTO donations VALUES (null,3, 1, 2, 2);
 
 INSERT INTO donations_categories VALUES (1, 2),(2,4),(2,1);
 
-INSERT INTO user_donations VALUES (1,1);
-INSERT INTO user_donations VALUES (2,2);
+# INSERT INTO user_donations VALUES (1,1);
+# INSERT INTO user_donations VALUES (2,2);
 
 INSERT INTO categories values (null, 'ubrania, które nadają się do ponownego użycia'), (null, 'ubrania, do wyrzucenia'), (null, 'zabawki'), (null, 'książki'), (null, 'inne');
+
+select d.id, d.bags_quantity, d.institution_id, d.shipping_address_id from donations as d straight_join users as u on d.user_id = u.id where user_id=2
