de624662c8ce3a08439ef9636bbe768f1c41b933
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-07-15 19:00:36 +0200

    DonationServiceImpl create method overridden

--- src/main/java/pl/coderslab/charity/service/impl/DonationServiceImpl.java
+++ src/main/java/pl/coderslab/charity/service/impl/DonationServiceImpl.java
@@ -3,6 +3,7 @@ package pl.coderslab.charity.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.coderslab.charity.entity.Donation;
+import pl.coderslab.charity.entity.ShippingAddress;
 import pl.coderslab.charity.service.impl.generic.GenericServiceImpl;
 import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.service.DonationService;
@@ -30,4 +31,15 @@ public class DonationServiceImpl extends GenericServiceImpl<Donation, DonationRe
         return repository.getDonationsByUserId(userId);
     }
 
+    @Override
+    public Donation create(Donation entity) {
+        ShippingAddress shippingAddress = entity.getShippingAddress();
+        shippingAddress.setStreet(shippingAddress.getStreet().trim());
+        shippingAddress.setCity(shippingAddress.getCity().trim());
+        shippingAddress.setZipCode(shippingAddress.getZipCode().trim());
+        shippingAddress.setPhoneNumber(shippingAddress.getPhoneNumber().trim());
+        shippingAddress.setPickUpComment(shippingAddress.getPickUpComment().trim());
+        entity.setShippingAddress(shippingAddress);
+        return repository.save(entity);
+    }
 }
