391cbce1f35fbb5bb167851b8149d3b8c41068ed
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-08-04 18:02:22 +0200

    User.java isEnabled added

--- src/main/java/pl/coderslab/charity/entity/User.java
+++ src/main/java/pl/coderslab/charity/entity/User.java
@@ -17,21 +17,15 @@ public class User extends GenericEntityID {
     private String lastName;
     private String password;
     private String email;
+    private boolean isEnabled;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_authority",
             joinColumns = { @JoinColumn(name = "user_id") },
             inverseJoinColumns = { @JoinColumn(name = "authority_id") })
     private Set<Authority> authorities;
 
-//    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//    @JoinTable(name = "user_donations",
-//            joinColumns = { @JoinColumn(name = "user_id") },
-//            inverseJoinColumns = { @JoinColumn(name = "donation_id") })
-//    private Set<Donation> donations;
-
     public User () {
-//        this.donations = new HashSet<>();
         this.authorities = new HashSet<>();
     }
 
