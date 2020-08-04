be984b6b74e7ef0b4fc227811ff91f74cec34a46
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-08-04 17:54:20 +0200

    FacadeHomeService BindingResult removed from register()

--- src/main/java/pl/coderslab/charity/facade/FacadeHomeService.java
+++ src/main/java/pl/coderslab/charity/facade/FacadeHomeService.java
@@ -1,6 +1,5 @@
 package pl.coderslab.charity.facade;
 
-import org.springframework.validation.BindingResult;
 import pl.coderslab.charity.dto.DonationDTO;
 import pl.coderslab.charity.dto.InstitutionDTO;
 import pl.coderslab.charity.dto.UserDTO;
@@ -12,7 +11,7 @@ public interface FacadeHomeService {
     Set<InstitutionDTO> findAllInstitutions();
     Integer getTotalBags();
     Set<DonationDTO> findAllDonations();
-    void register(UserDTO userDTO, BindingResult bindingResult);
+    void register(UserDTO userDTO);
     UserDTO findUserByEmail(String email);
 
 }
