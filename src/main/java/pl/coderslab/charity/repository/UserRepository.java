35ebb45fe19ab71da00422912b521db25c5f4034
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-08-04 18:05:08 +0200

    UserRepository findByEmail query changed for enabled user

--- src/main/java/pl/coderslab/charity/repository/UserRepository.java
+++ src/main/java/pl/coderslab/charity/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.coderslab.charity.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    @Query(value = "SELECT u.id, email, first_name, is_enabled, last_name, password FROM users AS u WHERE u.email=:email AND u.is_enabled=true", nativeQuery = true)
+    User findByEmail(@Param("email") String email);
 
 }
