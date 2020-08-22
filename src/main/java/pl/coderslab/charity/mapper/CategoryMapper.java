a8412aae9874b22e86ec2d54540aa3c4cb5d3de6
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-08-22 21:19:33 +0200

    CategoryMapper cleaned up

--- src/main/java/pl/coderslab/charity/mapper/CategoryMapper.java
+++ src/main/java/pl/coderslab/charity/mapper/CategoryMapper.java
@@ -11,15 +11,7 @@ import java.util.Set;
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
-//    @Mappings({
-//        @Mapping(target = "id", source = "entity.id"),
-//        @Mapping(target = "name", source = "entity.name")
-//    })
     CategoryDTO mapToDTO(Category entity);
-//    @Mappings({
-//        @Mapping(target = "id", source = "dto.id"),
-//        @Mapping(target = "name", source = "dto.name")
-//    })
     Category mapToEntity(CategoryDTO dto);
 
     Set<Category> setCategoriesToEntities(Set<CategoryDTO> dtoSet);
